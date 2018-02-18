3025357e0398644be0d33fceb440ac1300f0963b
Graham Wihlidal <graham@wihlidal.ca>
2018-02-18 18:16:45 +0100

    :fire: Removed S3 logic from client (exclusively use microservice now)

--- bin/client.rs
+++ bin/client.rs
@@ -2,9 +2,6 @@ extern crate futures;
 extern crate grpc;
 extern crate cloudstore;
 extern crate file;
-extern crate rusoto_core;
-extern crate rusoto_s3;
-extern crate rusoto_credentials;
 
 use std::io::{BufReader, Read};
 use std::fs::File;
@@ -12,11 +9,6 @@ use std::env;
 
 use cloudstore::cloudstore_grpc::*;
 use cloudstore::cloudstore::*;
-
-use rusoto_core::{default_tls_client, DefaultCredentialsProvider, Region};
-use rusoto_s3::*;
-
-const BUCKET_NAME: &'static str = "p4content";
 
 fn main() {
 
@@ -58,36 +50,4 @@ fn main() {
     delete_req.set_filename(test_file.clone());
     let delete_res = client.delete(grpc::RequestOptions::new(), delete_req);
     println!("{:?}", delete_res.wait());
-
-    /*let region = Region::Custom {
-        name: "hcy-minio".to_owned(),
-        endpoint: "http://192.168.1.69:9000".to_owned(),
-    };*/
-
-    //let provider = StaticProvider::new_minimal("AJ20P3XYDOURW7WZSHJ1".to_owned(), "EVT9XzEw/77PevdntA88wjcEBF2cANl/Duc09mkl".to_owned())
-    //            .credentials().wait();
-    /*let param_provider: Option<ParametersProvider>;
-    param_provider = Some(
-        ParametersProvider::with_params(
-            "AJ20P3XYDOURW7WZSHJ1",
-            "EVT9XzEw/77PevdntA88wjcEBF2cANl/Duc09mkl",
-            None).unwrap()
-    );*/
-
-    //let provider = DefaultCredentialsProvider::new(param_provider).unwrap();
-
-    //let tls_client = default_tls_client().unwrap();
-    //let provider = DefaultCredentialsProvider::new().unwrap();
-   // let credentials = DefaultCredentialsProvider::new().unwrap();
-    //let s3 = rusoto_s3::S3Client::new(tls_client, provider, region);
-    //let s3 = rusoto_s3::S3Client::simple(region);
-
-    /*let mut request = rusoto_s3::PutObjectRequest::default();
-    request.key = test_file.to_string();
-    request.bucket = "p4content".to_string();
-    request.body = Some(bytes);
-    request.content_type = Some("text/plain".to_string());
-
-    let _result = s3.put_object(&request);*/
-
 }
